d7c20772ebc6a5d3cd14bbbc794de025052d8733
Kevin D. Wolf <kevinw@software-logistics.com>
2017-12-01 15:28:17 -0500

    added test/updated all nugets

--- src/LagoVista.IoT.Deployment.Admin/Models/UsageMetrics.cs
+++ src/LagoVista.IoT.Deployment.Admin/Models/UsageMetrics.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using LagoVista.Core;
+using System.Diagnostics;
 
 namespace LagoVista.IoT.Deployment.Admin.Models
 {
@@ -68,6 +69,56 @@ namespace LagoVista.IoT.Deployment.Admin.Models
         [JsonProperty("processingMS")]
         public double ProcessingMS { get; set; }
 
+        public void Concat(UsageMetrics metric)
+        {
+            Debug.Assert(metric == null, "Must provide a value for metric to be clone");
+
+            MessagesProcessed += metric.MessagesProcessed;
+            ErrorCount += metric.ErrorCount;
+            BytesProcessed += metric.BytesProcessed;
+            WarningCount += metric.WarningCount;
+            ProcessingMS += metric.ProcessingMS;
+            DeadLetterCount += metric.DeadLetterCount;
+            ActiveCount += metric.ActiveCount;
+        }
+
+        public UsageMetrics Clone()
+        {
+            var clonedMetric = new UsageMetrics()
+            {
+                Version = Version,
+                HostId = HostId,
+                InstanceId = InstanceId,
+                PipelineModuleId = PipelineModuleId,
+
+                MessagesProcessed = MessagesProcessed,
+                ErrorCount = ErrorCount,
+                BytesProcessed = BytesProcessed,
+                WarningCount = WarningCount,
+                ProcessingMS = ProcessingMS,
+                DeadLetterCount = DeadLetterCount,
+                ActiveCount = ActiveCount,
+            };
+
+            return clonedMetric;
+        }
+
+        public void Calculate()
+        {
+            ElapsedMS = Math.Round((EndTimeStamp.ToDateTime() - StartTimeStamp.ToDateTime()).TotalMilliseconds, 3);
+
+            if (ElapsedMS > 1)
+            {
+                MessagesPerSecond = (MessagesProcessed / ElapsedMS) * 1000.0;
+            }
+
+            if (MessagesProcessed > 0)
+            {
+                AvergeProcessingMs = Math.Round(ProcessingMS / MessagesProcessed, 3);
+            }
+        }
+
+
         public void Reset(String previousEndTime = null)
         {
             StartTimeStamp = previousEndTime == null ? DateTime.UtcNow.ToJSONString() : previousEndTime;
@@ -79,9 +130,11 @@ namespace LagoVista.IoT.Deployment.Admin.Models
             BytesProcessed = 0;
             ErrorCount = 0;
             WarningCount = 0;
-            ActiveCount = 0;
             ProcessingMS = 0;
             DeadLetterCount = 0;
+
+            /* Don't reset ActiveCount, this is carried between metrics and is always live */
+            //ActiveCount = NA;
         }
     }
 }
---
+++ tests/LagoVista.IoT.Deployment.Tests/UsageMetrics/UsageMetricsTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LagoVista.IoT.Deployment.Admin.Models;
+using System;
+using LagoVista.Core;
+
+namespace LagoVista.IoT.Deployment.Tests
+{
+    [TestClass]
+    public class UsageMetricsTests
+    {
+        [TestMethod]
+        public void UsageMetrics_Concat()
+        {
+            var childMetricsmetric = new UsageMetrics();
+            childMetricsmetric.BytesProcessed = 12345;
+            childMetricsmetric.DeadLetterCount = 25;
+            childMetricsmetric.ErrorCount = 5;
+            childMetricsmetric.WarningCount = 12;
+            childMetricsmetric.ProcessingMS = 1235;
+            childMetricsmetric.MessagesProcessed = 1235;
+            childMetricsmetric.ActiveCount = 19;
+
+            var rolledUpMetrics = new UsageMetrics();
+            rolledUpMetrics.BytesProcessed = 500;
+            rolledUpMetrics.DeadLetterCount = 20;
+            rolledUpMetrics.ErrorCount = 38;
+            rolledUpMetrics.MessagesProcessed = 20;
+            rolledUpMetrics.ProcessingMS = 123123;
+            rolledUpMetrics.ActiveCount = 38;
+            rolledUpMetrics.WarningCount = 15;
+
+            rolledUpMetrics.Concat(childMetricsmetric);
+
+            Assert.AreEqual(childMetricsmetric.BytesProcessed + 500, rolledUpMetrics.BytesProcessed);
+            Assert.AreEqual(childMetricsmetric.DeadLetterCount + 20, rolledUpMetrics.DeadLetterCount);
+            Assert.AreEqual(childMetricsmetric.ErrorCount + 38, rolledUpMetrics.ErrorCount);
+            Assert.AreEqual(childMetricsmetric.MessagesProcessed + 20, rolledUpMetrics.MessagesProcessed);
+            Assert.AreEqual(childMetricsmetric.ProcessingMS + 123123, rolledUpMetrics.ProcessingMS);
+            Assert.AreEqual(childMetricsmetric.WarningCount + 15, rolledUpMetrics.WarningCount);
+            Assert.AreEqual(childMetricsmetric.ActiveCount + 38, rolledUpMetrics.ActiveCount);
+        }
+
+        [TestMethod]
+        public void UsageMetrics_Clone()
+        {
+            var metricToClone = new UsageMetrics();
+            metricToClone.Version = "1.2.3.4";
+            metricToClone.HostId = "hostid";
+            metricToClone.InstanceId = "instanceid";
+            metricToClone.PipelineModuleId = "piplinemoduleid";
+
+            metricToClone.BytesProcessed = 12345;
+            metricToClone.DeadLetterCount = 25;
+            metricToClone.ErrorCount = 5;
+            metricToClone.WarningCount = 12;
+            metricToClone.ProcessingMS = 1235;
+            metricToClone.MessagesProcessed = 1235;
+            metricToClone.ActiveCount = 19;
+
+            var clonedMetric = metricToClone.Clone();
+
+            Assert.IsTrue(String.IsNullOrEmpty(clonedMetric.PartitionKey));
+            Assert.IsTrue(String.IsNullOrEmpty(clonedMetric.RowKey));
+            Assert.IsTrue(String.IsNullOrEmpty(clonedMetric.StartTimeStamp));
+            Assert.IsTrue(String.IsNullOrEmpty(clonedMetric.EndTimeStamp));
+
+            Assert.AreEqual("1.2.3.4", clonedMetric.Version);
+            Assert.AreEqual("hostid", clonedMetric.HostId);
+            Assert.AreEqual("instanceid", clonedMetric.InstanceId);
+            Assert.AreEqual("piplinemoduleid", clonedMetric.PipelineModuleId);
+            Assert.AreEqual(1235, clonedMetric.MessagesProcessed);
+            Assert.AreEqual(5, clonedMetric.ErrorCount);
+            Assert.AreEqual(12345, clonedMetric.BytesProcessed);
+            Assert.AreEqual(12, clonedMetric.WarningCount);
+            Assert.AreEqual(1235, clonedMetric.ProcessingMS);
+            Assert.AreEqual(25, clonedMetric.DeadLetterCount);
+            Assert.AreEqual(19, clonedMetric.ActiveCount);            
+        }
+
+        [TestMethod]
+        public void UsageMetrics_Calculate_MessagePerSecond()
+        {
+            var childMetricsmetric = new UsageMetrics();
+            var end = DateTime.Now;
+            childMetricsmetric.EndTimeStamp = end.ToJSONString();
+            childMetricsmetric.StartTimeStamp = end.AddMinutes(-1).ToJSONString();
+            childMetricsmetric.BytesProcessed = 12345;
+
+            childMetricsmetric.ElapsedMS = 60 * 1000;
+            childMetricsmetric.ProcessingMS = 1235;
+            childMetricsmetric.MessagesProcessed = 120;
+
+            childMetricsmetric.Calculate();
+
+            Assert.AreEqual(2, childMetricsmetric.MessagesPerSecond);
+        }
+
+        [TestMethod]
+        public void UsageMetrics_Calculate_ElapsedMS()
+        {
+            var childMetricsmetric = new UsageMetrics();
+            var end = DateTime.Now;
+            childMetricsmetric.EndTimeStamp = end.ToJSONString();
+            childMetricsmetric.StartTimeStamp = end.AddMinutes(-1).ToJSONString();
+
+            childMetricsmetric.Calculate();
+
+            Assert.AreEqual(60 * 1000, childMetricsmetric.ElapsedMS, 0.1);
+        }
+
+
+        [TestMethod]
+        public void UsageMetrics_Calculate_AvergeProcessing()
+        {
+            var childMetricsmetric = new UsageMetrics();
+            var end = DateTime.Now;
+            childMetricsmetric.EndTimeStamp = end.ToJSONString();
+            childMetricsmetric.StartTimeStamp = end.AddMinutes(-1).ToJSONString();
+            childMetricsmetric.BytesProcessed = 12345;
+
+            childMetricsmetric.ProcessingMS = 2400;
+            childMetricsmetric.MessagesProcessed = 120;
+
+            childMetricsmetric.Calculate();
+
+            Assert.AreEqual(20, childMetricsmetric.AvergeProcessingMs);
+        }
+    }
+}
