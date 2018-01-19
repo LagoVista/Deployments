7d673a69a13b85ea18836b0636b6fb685971d99f
Kevin D. Wolf <kevinw@software-logistics.com>
2018-01-19 13:23:07 -0500

    fix compile error

--- src/LagoVista.IoT.Deployment.Admin/Services/DeviceManagementConnectorService.cs
+++ src/LagoVista.IoT.Deployment.Admin/Services/DeviceManagementConnectorService.cs
@@ -94,5 +94,11 @@ namespace LagoVista.IoT.Deployment.Admin.Services
             var uri = $"/api/devices/config/{configurationId}/full";
             return GetListResponseAsync<Device>(uri, instanceId, org, user, listRequest);
         }
+
+        public Task<ListResponse<DeviceSummary>> GetDevicesInCustomStatusAsync(string instanceId, string status, ListRequest listRequest, EntityHeader org, EntityHeader user)
+        {
+            var uri = $"/api/devices/customstatus/{status}";
+            return GetListResponseAsync<DeviceSummary>(uri, instanceId, org, user, listRequest);
+        }
     }
 }
