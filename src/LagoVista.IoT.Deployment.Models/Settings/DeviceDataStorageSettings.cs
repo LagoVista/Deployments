de23104015604696de925055fb28460d21b7e713
Kevin D. Wolf <kevinw@software-logistics.com>
2020-06-15 18:36:41 -0400

    add settings for error/status storage

--- src/LagoVista.IoT.Deployment.Models/Settings/DeviceDataStorageSettings.cs
+++ src/LagoVista.IoT.Deployment.Models/Settings/DeviceDataStorageSettings.cs
@@ -5,6 +5,8 @@ namespace LagoVista.IoT.Deployment.Models.Settings
     public class DeviceDataStorageSettings
     {
         public ConnectionSettings DeviceArchiveTableStorage { get; set; }
+        public ConnectionSettings DeviceExceptionTableStorage { get; set; }
+        public ConnectionSettings DeviceStatusTableStorage { get; set; }
         public ConnectionSettings DeviceMediaTableStorage { get; set; }
         public ConnectionSettings DeviceMediaBlobStorage { get; set; }
 
