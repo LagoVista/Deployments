3d807628913fc5b99119e17a07509b537d40c92c
Kevin D. Wolf <kevinw@software-logistics.com>
2020-06-30 08:08:43 -0400

    add device unqiue id to create ticket request

--- src/LagoVista.IoT.Deployment.Models/CreateServiceTicketRequest.cs
+++ src/LagoVista.IoT.Deployment.Models/CreateServiceTicketRequest.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LagoVista.IoT.Deployment.Models
+﻿namespace LagoVista.IoT.Deployment.Models
 {
     public class CreateServiceTicketRequest
     {
         public string BoardId { get; set; }
         public string TemplateId { get; set; }
         public string RepoId { get; set; }
+        public string DeviceUniqueId { get; set; }
         public string DeviceId { get; set; }
         public string Subject { get; set; }
         public string Details { get; set; }
