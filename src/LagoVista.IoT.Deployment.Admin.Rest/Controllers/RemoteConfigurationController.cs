34207bfaf4b26c9237af619f5b8481aa12a6ca02
Kevin D. Wolf <kevinw@software-logistics.com>
2024-08-17 18:17:35 -0400

    methods to send device commands

--- src/LagoVista.IoT.Deployment.Admin.Rest/Controllers/RemoteConfigurationController.cs
+++ src/LagoVista.IoT.Deployment.Admin.Rest/Controllers/RemoteConfigurationController.cs
@@ -10,6 +10,10 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Reflection.Metadata;
+using LagoVista.IoT.DeviceManagement.Core.Managers;
+using LagoVista.Core.Models;
+using System;
+using LagoVista.Core;
 
 namespace LagoVista.IoT.Deployment.Admin.Rest.Controllers
 {
@@ -22,24 +26,44 @@ namespace LagoVista.IoT.Deployment.Admin.Rest.Controllers
     public class RemoteConfigurationController : LagoVistaBaseController
     {
         IRemoteConfigurationManager _remoteConfigMgr;
+        IDeviceRepositoryManager _repoManager;
+        IDeviceManager _deviceManager;
 
-        public RemoteConfigurationController(IRemoteConfigurationManager remoteConfigMgr, UserManager<AppUser> userManager, IAdminLogger logger)
+        public RemoteConfigurationController(IRemoteConfigurationManager remoteConfigMgr, IDeviceRepositoryManager repoManager, IDeviceManager deviceManager, UserManager<AppUser> userManager, IAdminLogger logger)
             : base(userManager, logger)
         {
             _remoteConfigMgr = remoteConfigMgr;
+            _repoManager = repoManager;
+            _deviceManager = deviceManager;
         }
 
-        [HttpGet("/api/device/remoteconfig/{repoid}/{id}/all/send")]
-        public Task<InvokeResult> SendAllRemotePropertiesAsync(string repoid, string id)
+        [HttpGet("/api/device/remoteconfig/{orgid}/{repoid}/{id}/{pin}/all/send")]
+        public async Task<InvokeResult> SendAllRemotePropertiesAsync(string orgid, string repoid, string id, string pin)
         {
-            return _remoteConfigMgr.SendAllPropertiesAsync(repoid, id, OrgEntityHeader, UserEntityHeader);
+            var org = EntityHeader.Create(orgid, "PIN Device Access");
+            var user = EntityHeader.Create(Guid.Empty.ToId(), "PIN Device Access");
+
+            var repo = await _repoManager.GetDeviceRepositoryWithSecretsAsync(repoid, org, user, pin);
+            var result = await _deviceManager.GetDeviceByIdWithPinAsync(repo, id, pin, org, user);
+            if (result.Successful)
+                return await _remoteConfigMgr.SendAllPropertiesAsync(repoid, id, org, user);
+            else
+                return result.ToInvokeResult();
         }
 
 
-        [HttpGet("/api/device/remoteconfig/{repoid}/{id}/{idx}/send")]
-        public Task<InvokeResult> SendRemotePropertyAsync(string repoid, string id, int idx)
+        [HttpGet("/api/device/remoteconfig/{orgid}/{repoid}/{id}/{pin}/{idx}/send")]
+        public async Task<InvokeResult> SendRemotePropertyAsync(string orgid, string repoid, string id, string pin, int idx)
         {
-            return _remoteConfigMgr.SendPropertyAsync(repoid, id, idx, OrgEntityHeader, UserEntityHeader);
+            var org = EntityHeader.Create(orgid, "PIN Device Access");
+            var user = EntityHeader.Create(Guid.Empty.ToId(), "PIN Device Access");
+
+            var repo = await _repoManager.GetDeviceRepositoryWithSecretsAsync(repoid, org, user, pin);
+            var result = await _deviceManager.GetDeviceByIdWithPinAsync(repo, id, pin, org, user);
+            if (result.Successful)
+                return await _remoteConfigMgr.SendPropertyAsync(repoid, id, idx, org, user);
+            else
+                return result.ToInvokeResult();
         }
 
 
@@ -50,9 +74,24 @@ namespace LagoVista.IoT.Deployment.Admin.Rest.Controllers
         }
 
         [HttpPost("/api/device/remoteconfig/{repoid}/{id}/command/{cmdid}")]
-        public Task<InvokeResult> SendRemoteCommand(string repoid, string id, string cmdid, [FromBody] List<KeyValuePair<string, string>> parameters)
-        {
+            public Task<InvokeResult> SendRemoteCommand(string repoid, string id, string cmdid, [FromBody] List<KeyValuePair<string, string>> parameters)
+            {
             return _remoteConfigMgr.SendCommandAsync(repoid, id, cmdid, parameters, OrgEntityHeader, UserEntityHeader);
+        }
+
+        [HttpPost("/api/device/remoteconfig/{orgid}/{repoid}/{id}/{pin}/command/{cmdid}")]
+        public async Task<InvokeResult> SendRemoteCommand(string orgid, string repoid, string id, string pin, string cmdid, [FromBody] List<KeyValuePair<string, string>> parameters)
+        {
+            var org = EntityHeader.Create(orgid, "PIN Device Access");
+            var user = EntityHeader.Create(Guid.Empty.ToId(), "PIN Device Access");
+
+
+            var repo = await _repoManager.GetDeviceRepositoryWithSecretsAsync(repoid, org, user, pin);
+            var result = await _deviceManager.GetDeviceByIdWithPinAsync(repo, id, pin, org, user);
+            if (result.Successful)
+                return await _remoteConfigMgr.SendCommandAsync(repoid, id, cmdid, parameters, org, user);
+            else
+                return result.ToInvokeResult();
         }
 
         [HttpGet("/api/device/remoteconfig/{repoid}/{id}/restart")]
