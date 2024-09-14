a0151d3fd9c828bdb2e5a3ffc583eaad0b236016
Kevin Wolf <kevinw@software-logistics.com>
2024-09-14 11:51:09 -0500

    TAK server interface

---
+++ src/LagoVista.IoT.Deployment.Admin/Interfaces/ITakServer.cs
@@ -0,0 +1,25 @@
+﻿using LagoVista.Core.Validation;
+using LagoVista.IoT.DeviceMessaging.Models.Cot;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.Deployment.Admin
+{
+    public interface ITakClient
+    {
+        event EventHandler TakServerConnected;
+        event EventHandler TakServerDisconnected;
+        event EventHandler<Event> MessageReceived;
+
+        Task<InvokeResult> InitAsync(string dataPackageResourceId);
+        Task<InvokeResult> InitAsync(Stream stream);
+        Task<bool> Connectd();
+        Task<InvokeResult> ConnectAsync();
+        Task<InvokeResult> ListenAsync(CancellationToken cancellationToken = default);
+        Task<InvokeResult> DisconnectAsync();
+        Task<InvokeResult> SendAsync(Message message, CancellationToken cancellationToken = default);
+        Task<InvokeResult> SendAsync(string rawXml, CancellationToken cancellationToken = default);
+    }
+}
