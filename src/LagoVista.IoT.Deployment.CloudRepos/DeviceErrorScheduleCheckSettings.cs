c4dc8ad78043a09ab5e4212920051efba43c869c
Kevin Wolf <kevinw@software-logistics.com>
2026-03-25 05:25:07 -0400

    add registration section

--- src/LagoVista.IoT.Deployment.CloudRepos/DeviceErrorScheduleCheckSettings.cs
+++ src/LagoVista.IoT.Deployment.CloudRepos/DeviceErrorScheduleCheckSettings.cs
@@ -1,10 +1,24 @@
 ﻿using LagoVista.Core.Interfaces;
+using LagoVista.Core.Models;
 using LagoVista.IoT.Deployment.Admin.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace LagoVista.IoT.Deployment.CloudRepos
 {
     public class DeviceErrorScheduleCheckSettings : IDeviceErrorScheduleCheckSettings
     {
         public IConnectionSettings DeviceErrorScheduleQueueSettings { get; }
+
+        public DeviceErrorScheduleCheckSettings(IConfiguration configuration)
+        {
+            var section = configuration.GetRequiredSection("ErrorHandlerQueue");
+            DeviceErrorScheduleQueueSettings = new ConnectionSettings()
+            {
+                AccountId = section.Require("AccountId"),
+                UserName = section.Require("AccessKeyName"),
+                AccessKey = section.Require("AccessKey"),
+                ResourceName = section.Require("QueueName"),
+            };
+        }
     }
 }
